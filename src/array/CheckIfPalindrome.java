3b2e2858e9d8f8b1d288fabd94a60876be2a7a8a
Khemrath <khemrathseng@gmail.com>
2024-07-21 16:40:15 +0700

    DSA JAVA: - Array  + Check if word is palindrome

--- src/array/CheckIfPalindrome.java
+++ src/array/CheckIfPalindrome.java
@@ -1,13 +1,15 @@
 package array;
 
+import static array.ArrayUtil.printArray;
+
 public class CheckIfPalindrome {
 
-    public static boolean isPalindrome(String word) {
+    public static boolean checkIfPalindrome(String word) {
         char[] wordArray = word.toCharArray();
         int start = 0;
         int end = wordArray.length - 1;
-        while (start < end) {
-            if (wordArray[start] != wordArray[end]) {
+        while(start < end) {
+            if(wordArray[start] != wordArray[end]) {
                 return false;
             }
             start++;
@@ -19,7 +21,7 @@ public class CheckIfPalindrome {
     public static void main() {
         String word = "madam";
         System.out.println(word);
-        boolean isPalindrome = isPalindrome(word);
+        boolean isPalindrome = checkIfPalindrome(word);
         System.out.println(isPalindrome);
     }
 
